86cfe2e8b3ab677623d564a1d68885a41ee4f02f
dskoolkarni <30574252+dskoolkarni@users.noreply.github.com>
2017-09-24 19:15:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'article-one.html'));
 });
 
 app.get('/artical-two', function (req, res) {
